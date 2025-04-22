0bffecfafa8829eedecfef409efd08c2c9e68c5f
d-yv <yuri.dubrovin@gmail.com>
2025-04-22 12:01:43 +0300

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+const submitData = {};
+const createSubmitData = (e) => {
+  e.preventDefault();
 
+  const form = e.target.elements;
+  const email = form.email.value.trim();
+  const password = form.password.value.trim();
+
+  if (email != "" && password != "") {
+    submitData.email = email;
+    submitData.password = password;
+  } else {
+    return alert("All form fields must be filled in");
+  }
+  e.target.reset();
+  return console.log(submitData);
+};
+loginForm.addEventListener("submit", createSubmitData);
