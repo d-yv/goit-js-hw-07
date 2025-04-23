35e885cf3b22e86cd0b002218dd49d6067f84a83
d-yv <yuri.dubrovin@gmail.com>
2025-04-23 23:15:02 +0300

    correct

--- js/task-1.js
+++ js/task-1.js
@@ -1,20 +1,7 @@
-const listCategories = document.querySelectorAll(".item");
-console.log("Number of categories:", listCategories.length);
+const categories = document.querySelectorAll(".item");
+console.log("Number of categories:", categories.length);
 
-const textTitle = document.querySelectorAll("h2");
-const elements = document.querySelectorAll(".item ul");
-
-textTitle.forEach((title, key) => {
-  console.log("Category: ", title.textContent);
-  console.log("Elements:", elements[key].childElementCount);
-
-  title.setAttribute("class", "title");
-  elements[key].setAttribute("class", "item-list");
-});
-
-document.querySelector("#categories").setAttribute("class", "categories-list");
-
-const listElements = document.querySelectorAll(".item ul li");
-listElements.forEach((element) => {
-  element.setAttribute("class", "item-list-element");
+categories.forEach((title) => {
+  console.log("Category: ", title.children[0].textContent);
+  console.log("Elements:", title.children[1].childElementCount);
 });
